834deb9f83d327a3cc88af6c94732ecb1a7f9a81
tiway <stilon670@gmail.com>
2024-09-01 17:16:06 +0300

    add read_temp_for_city to router "Temperature"

--- app/temperature/router.py
+++ app/temperature/router.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.city.models import DBCity
 from app.dependencies import get_session
 from app.temperature import schemas
-from app.temperature.crud import create_temp_record, get_temp
+from app.temperature.crud import create_temp_record, get_all_temp, get_temp_for_city
 from app.temperature.weather import fetch_temperature_data
 
 router = APIRouter(tags=["Temperature"])
@@ -31,5 +31,10 @@ async def temp_update(db: AsyncSession = Depends(get_session)):
 
 
 @router.get("/", response_model=list[schemas.TemperatureRead])
-async def read_temp(db: AsyncSession = Depends(get_session)):
-    return await get_temp(db=db)
+async def read_all_temperatures(db: AsyncSession = Depends(get_session)):
+    return await get_all_temp(db=db)
+
+
+@router.get("/{city_id}/", response_model=list[schemas.TemperatureRead])
+async def read_temp_for_city(city_id: int, db: AsyncSession = Depends(get_session)):
+    return await get_temp_for_city(id_city=city_id, db=db)
