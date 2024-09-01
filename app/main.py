7b29d5d71e85b7c7d518603d5017ecd1ec3f5e27
tiway <stilon670@gmail.com>
2024-09-01 16:20:38 +0300

    add router temperature to main.py

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ import asyncio
 from fastapi import FastAPI
 
 
-from app.city.router import router
+from app.city.router import router as city_router
+from app.temperature.router import router as temp_router
 
 app = FastAPI()
 
 
-app.include_router(router, prefix="/city")
+app.include_router(city_router, prefix="/city")
+app.include_router(temp_router, prefix="/temperatures")
 
 
 @app.get("/")
