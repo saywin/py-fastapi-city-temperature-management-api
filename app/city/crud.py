23ca99fb077d23cf562873ced1d4527af134c3c4
tiway <stilon670@gmail.com>
2024-09-01 10:57:36 +0300

    Add create_city crud

--- app/city/crud.py
+++ app/city/crud.py
@@ -1,15 +1,20 @@
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.city.schemas import City
+from app.city.models import DBCity
+from app.city.schemas import City, CityCreate
 
 
-async def get_all_city(db: AsyncSession) -> list[City]:
-    db_city = await db.execute(select(City))
-    return db_city.scalar().all()
+async def get_all_city(db: AsyncSession) -> [City]:
+    result = await db.execute(select(DBCity))
+    db_cities = result.scalars().all()
+    return db_cities
 
 
-def create_city(db: AsyncSession, city: str, additional_info: str):
-    new_city = City(city=city, additional_info=additional_info)
+async def create_city(db: AsyncSession, city: CityCreate):
+    new_city = DBCity(**city.dict())
     db.add(new_city)
+    await db.commit()
+    await db.refresh(new_city)
     return new_city
