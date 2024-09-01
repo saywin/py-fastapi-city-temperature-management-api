fdd2334bd7e9d857c4ccca28e89ad7ad33d0ad32
tiway <stilon670@gmail.com>
2024-09-01 12:29:59 +0300

    Add models.py with Temperature

--- app/city/models.py
+++ app/city/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.db import Base
+from app.temperature.models import DBTemperature
 
 
 class DBCity(Base):
@@ -9,3 +11,5 @@ class DBCity(Base):
     id = Column(Integer, index=True, primary_key=True)
     city = Column(String(255), unique=True, index=True)
     additional_info = Column(String(510))
+
+    temperature = relationship(DBTemperature, back_populates="cities")
---
+++ app/temperature/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, Float
+from sqlalchemy.orm import relationship
+
+from app.city.models import DBCity
+from app.db import Base
+
+
+class DBTemperature(Base):
+    __tablename__ = "temperature"
+
+    id = Column(Integer, primary_key=True, index=True)
+    city_id = Column(Integer, ForeignKey(DBCity.id))
+    date_time = Column(DateTime)
+    temperature = Column(Float)
+
+    cities = relationship(DBCity, back_populates="temperature")
