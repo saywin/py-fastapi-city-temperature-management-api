99e53d7b1f2cd6c312c5478c6f71ac3f1edaf6fc
tiway <stilon670@gmail.com>
2024-09-01 16:49:20 +0300

    changed update_temperature on create_temp_record

--- app/temperature/crud.py
+++ app/temperature/crud.py
@@ -6,18 +6,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.temperature.models import DBTemperature
 
 
-async def get_temperature_by_city_id(db: AsyncSession, city_id: int) -> DBTemperature:
-    find_temperature_by_id = select(DBTemperature).where(DBTemperature.city_id == city_id)
-    result = await db.execute(find_temperature_by_id)
-    return result.scalars().first()
-
-
-async def update_temperature(db: AsyncSession, city_id: int, new_temperature: float) -> DBTemperature:
-    temp_entry = await get_temperature_by_city_id(db, city_id)
-    if temp_entry:
-        temp_entry.temperature = new_temperature
-        temp_entry.date_time = datetime.utcnow()
+async def create_temp_record(db: AsyncSession, city_id: int, temp: float) -> DBTemperature:
+    async with db:
+        temp_entry = DBTemperature(
+            city_id=city_id,
+            temperature=temp,
+            date_time=datetime.utcnow()
+        )
         db.add(temp_entry)
         await db.commit()
         await db.refresh(temp_entry)
-    return temp_entry
+        return temp_entry
