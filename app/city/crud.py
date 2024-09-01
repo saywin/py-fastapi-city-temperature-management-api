0f3c1a8eb19baa60bb7951de77a012d5097e44f9
tiway <stilon670@gmail.com>
2024-09-01 11:53:23 +0300

    Add get_city to crud

--- app/city/crud.py
+++ app/city/crud.py
@@ -2,18 +2,28 @@ from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.city import schemas
 from app.city.models import DBCity
 from app.city.schemas import City, CityCreate
 
 
 async def get_all_city(db: AsyncSession) -> [City]:
     result = await db.execute(select(DBCity))
-    db_cities = result.scalars().all()
-    return db_cities
+    cities = result.scalars().all()
+    return cities
 
 
-async def create_city(db: AsyncSession, city: CityCreate):
+async def get_city(city_id: int, db: AsyncSession):
+    find_city = select(DBCity).where(DBCity.id == city_id)
+    result = await db.execute(find_city)
+    city = result.scalar_one_or_none()
+
+    if city is None:
+        raise HTTPException(status_code=404, detail="City not found")
+
+    return city
+
+
+async def create_city(db: AsyncSession, city: CityCreate) -> DBCity:
     new_city = DBCity(**city.dict())
     db.add(new_city)
     await db.commit()
@@ -21,16 +31,15 @@ async def create_city(db: AsyncSession, city: CityCreate):
     return new_city
 
 
-async def update_city(db: AsyncSession, city_id: int, new_city: str, additional_info: str):
+async def update_city(
+    db: AsyncSession, city_id: int, new_city: str, additional_info: str
+) -> DBCity:
     find_city = select(DBCity).where(DBCity.id == city_id)
     result = await db.execute(find_city)
     city = result.scalar_one_or_none()
 
     if city is None:
-        raise HTTPException(
-            status_code=404,
-            detail="City not found"
-        )
+        raise HTTPException(status_code=404, detail="City not found")
 
     city.city = new_city
     city.additional_info = additional_info
@@ -39,20 +48,15 @@ async def update_city(db: AsyncSession, city_id: int, new_city: str, additional_
     return city
 
 
-async def delete_city(city_id: int, db: AsyncSession):
+async def delete_city(city_id: int, db: AsyncSession) -> DBCity:
     find_city = select(DBCity).where(DBCity.id == city_id)
     result = await db.execute(find_city)
     city = result.scalar_one_or_none()
 
     if city is None:
-        raise HTTPException(
-            status_code=404,
-            detail="City not found"
-        )
+        raise HTTPException(status_code=404, detail="City not found")
 
     await db.delete(city)
     await db.commit()
 
     return city
-
-
